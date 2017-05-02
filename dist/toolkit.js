56adb491ca0b997250806914720fa2f74daa36b7
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-05-02 10:58:09 -0400

    Add chunk functions for arrays

--- dist/toolkit.js
+++ dist/toolkit.js
@@ -70,16 +70,50 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-window._ = {
+window.__ = {
+
+  chunk: (array, size = 1) => {
+    const results = [];
+    let count = 0,
+        nestedArray = [],
+        currentElement;
+
+    for (let i = 0; i < array.length; i++) {
+      currentElement = array[i];
+      nestedArray.push(currentElement);
+      count += 1;
+
+      if (count === size) {
+        results.push(nestedArray);
+        nestedArray = [];
+        count = 0;
+      }
+    }
+
+    return results;
+
+  },
+
+  compact: (input) => {
+    return input.filter(element => {
+      return element;
+    });
+  },
+
   first: (input) => {
-    if (input.constructor === Array) {
+    if (input.constructor === Array || typeof input === "string") {
       return input[0];
-    } else {
-      return "Input for first() must be an array";
     }
   },
 
-  
+
+
+  // concat: (array) => {
+  //   const inputs = Array.prototype.slice.call(arguments);
+  //   debugger
+  // }
+
+
 
 };
 
